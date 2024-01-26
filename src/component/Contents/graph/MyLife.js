74ddbc72df67856872ac85e45e7c38fe08ec5811
maantano <maantano@naver.com>
2024-01-26 22:39:27 +0900

    chatbot & 썸네일 수정

--- src/component/Contents/graph/MyLife.js
+++ src/component/Contents/graph/MyLife.js
@@ -128,7 +128,7 @@ const MyLife = () => {
 
   return (
     <div className="flex mt-8 flex-col max-w-[1200px]">
-      <div className="text-gray-300 text-3xl">민경언의 인생곡선</div>
+      <div className="text-gray-300 text-3xl mb-5">민경언의 인생곡선</div>
       <img className="ml-4" src={myDevLife} alt="myDevLife" />
       <Graph data={tempData} />
     </div>
